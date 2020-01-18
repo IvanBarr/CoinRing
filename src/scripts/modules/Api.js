3e2449a0477fd8f075e3387e78c26bb8741954fc
IvanBarr <ivanbar.webdev@gmail.com>
2020-01-17 22:33:49 -0800

    added a function that calculates the dominance of top three coins and did a little of code refactoring

--- src/scripts/modules/Api.js
+++ src/scripts/modules/Api.js
@@ -1,43 +1,10 @@
 import MarketStatus from './MarketStatus'
 require('dotenv').config()
 
-// export const Api = {
-//     // url: "https://api.coingecko.com/api/v3/coins",
-//     // url: "https://api.nomics.com/v1/currencies/ticker?key=50d3530f3903fcf6af244ce0c86fe9e3",
-//     url: `https://api.nomics.com/v1/currencies/ticker?key=${process.env.API_KEY}&1d`,
-//     callApi() {
-//         fetch(this.url)
-//         .then(res => res.json())
-//         .then(data => {
-//             // buildBubbles(data)
-//             // updateBubbles(data)
-//             // this.refresh()
-//             console.log(data[2].symbol)
-//             let marketStatus = new MarketStatus()
-//         })
-//         .catch(err => {
-//             console.error(err)
-//         })
-//     },
-//     refresh(){
-//         setInterval(() => {
-//             fetch(this.url)
-//             .then(res => res.json())
-//             .then(data => {
-//                 // updateBubbles(data)
-//             })
-//             .catch(err => {
-//                 console.log("Failed to refesh")
-//                 console.error(err)
-//             })
-//         }, 10000);
-//     }
-// }
-
 class Api{
     constructor(){
-        // this.url = `https://api.nomics.com/v1/currencies/ticker?key=${process.env.API_KEY}&1d`
-        this.url = `https://api.nomics.com/v1/market-cap/history?key=${process.env.API_KEY}&start=2020-01-14T00%3A59%3A59.999Z`
+        this.url = `https://api.nomics.com/v1/currencies/ticker?key=${process.env.API_KEY}&1d`
+        // this.url = `https://api.nomics.com/v1/market-cap/history?key=${process.env.API_KEY}&start=2020-01-14T00%3A59%3A59.999Z`
 
         this.callApi(this.url)
         let marketStatus = new MarketStatus()
@@ -48,11 +15,7 @@ class Api{
         fetch(url)
             .then(res => res.json())
             .then(data => {
-                // buildBubbles(data)
-                // updateBubbles(data)
-                // this.refresh()
-                // console.log(data[2])
-                // let marketStatus = new MarketStatus()
+                // console.log(data[0].symbol)
             })
             .catch(err => {
                 console.error(err)
--- src/scripts/modules/MarketStatus.js
+++ src/scripts/modules/MarketStatus.js
@@ -2,30 +2,36 @@ require('dotenv').config()
 
 class MarketStatus{
     constructor(){
-        // this.year = new Date().getUTCFullYear()
-        // this.month = new Date().getUTCMonth() + 1
-        // this.day = new Date().getUTCDate()
-        // console.log(this.month)
+        // Global variables
+        this.totalMarketCap
+        this.yesterdaysDate = this.yesterdaysDate()
+        this.currentHour = this.currentHour()
 
-        this.marketCapUrl = `https://api.nomics.com/v1/market-cap/history?key=${process.env.API_KEY}&start=${this.currentDate()}T${this.currentHour()}%3A59%3A59.999Z`
-        this.volumeUrl = `https://api.nomics.com/v1/volume/history?key=${process.env.API_KEY}&start=${this.currentDate()}T${this.currentHour()}%3A59%3A59.999Z`
+        // URLS
+        this.coinsURL = `https://api.nomics.com/v1/currencies/ticker?key=${process.env.API_KEY}&1d`
+        this.marketCapUrl = `https://api.nomics.com/v1/market-cap/history?key=${process.env.API_KEY}&start=${this.yesterdaysDate}T${this.currentHour}%3A59%3A59.999Z`
+        this.volumeUrl = `https://api.nomics.com/v1/volume/history?key=${process.env.API_KEY}&start=${this.yesterdaysDate}T${this.currentHour}%3A59%3A59.999Z`
+        
+        // Eelements
         this.marketcap = document.querySelector('.market-status__marketcap')
         this.volume = document.querySelector('.market-status__volume')
+        this.dominanceElement = document.querySelector('.market-status__dominance')
 
+        // Sets the total cryptocurrency market cap
         this.callApi(this.marketCapUrl, this.setMarketCap)
+        // Sets the total 24 hour cryptocurrency volume
         this.callApi(this.volumeUrl, this.setVolume)
+        // Sets the top three dominant coins
+        this.callApi(this.coinsURL, this.setDominance)
 
     }
 
-    currentDate(){
+    yesterdaysDate(){
         let d = new Date();
-        let currentDate = new Date(2020, 0, 16);
+        let currentDate = new Date(d.getUTCFullYear(), d.getUTCMonth(), d.getUTCDate());
         let yesterday = currentDate.setDate(currentDate.getDate() - 1)
-        let nDate = new Date(yesterday)
-
-        console.log(nDate)
-
-        let dateString = d.getUTCFullYear() + "-" + ("0" + (d.getUTCMonth()+1)).slice(-2) + "-" + ("0" + d.getDate()).slice(-2)
+        let newDate = new Date(yesterday)
+        let dateString = newDate.getUTCFullYear() + "-" + ("0" + (newDate.getUTCMonth()+1)).slice(-2) + "-" + ("0" + newDate.getDate()).slice(-2)
         return dateString
     }
     
@@ -53,7 +59,10 @@ class MarketStatus{
     }
 
     setMarketCap(data){
-        this.marketcap.innerHTML = `$ ${this.formatNumber(data[22].market_cap)}`
+        let totalMarketcap = data[22].market_cap
+        this.marketcap.innerHTML = `$ ${this.formatNumber(totalMarketcap)}`
+        this.totalMarketCap = totalMarketcap
+        return totalMarketcap
     }
 
     setVolume(data){
@@ -63,6 +72,24 @@ class MarketStatus{
             totalVolume = totalVolume + parseInt(data[i].volume)
         }
         this.volume.innerHTML = `$ ${this.formatNumber(totalVolume)}`
+    }
+
+    setDominance(data){
+        let topThree = []
+
+        for(let i = 0; i < 3; i++){
+            let coinMarketCap = parseInt(data[i].market_cap)
+            topThree[i] = {
+                coinSymbol: data[i].symbol,
+                dominance: ((coinMarketCap / this.totalMarketCap) * 100).toFixed(2) + '%'
+            }
+
+            let ol = this.dominanceElement
+            let li = document.createElement('li')
+            let textNode = document.createTextNode(topThree[i].coinSymbol + '\xa0\xa0\xa0\xa0' + topThree[i].dominance)
+            li.appendChild(textNode)
+            ol.appendChild(li)
+        }
     }
 
 }
