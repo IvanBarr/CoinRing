35af284d65c874fbb819623577b4c57822b8fb17
IvanBarr <ivanbar.webdev@gmail.com>
2020-01-20 22:33:13 -0800

    added a new function that calculates the change in volume and market cap last 24 hours and another function that refreshes and updates the data on the page. Small changes to HTML and CSS.'

--- src/scripts/modules/Api.js
+++ src/scripts/modules/Api.js
@@ -3,23 +3,29 @@ require('dotenv').config()
 
 class Api{
     constructor(){
-        this.url = `https://api.nomics.com/v1/currencies/ticker?key=${process.env.API_KEY}&1d`
+        // this.url = `https://api.nomics.com/v1/currencies/ticker?key=${process.env.API_KEY}&1d`
         // this.url = `https://api.nomics.com/v1/market-cap/history?key=${process.env.API_KEY}&start=2020-01-14T00%3A59%3A59.999Z`
-
-        this.callApi(this.url)
-        let marketStatus = new MarketStatus()
+        // this.callApi(this.url)
+        new MarketStatus()
+        this.refresh()
     }
 
+    // callApi(url){
+    //     fetch(url)
+    //         .then(res => res.json())
+    //         .then(data => {
+    //             // console.log(data[0].symbol)
+    //         })
+    //         .catch(err => {
+    //             console.error(err)
+    //         })
+    // }
 
-    callApi(url){
-        fetch(url)
-            .then(res => res.json())
-            .then(data => {
-                // console.log(data[0].symbol)
-            })
-            .catch(err => {
-                console.error(err)
-            })
+    refresh(){
+        setInterval(() => {
+            new MarketStatus()
+            // console.log(this)
+        }, 10000)
     }
 
 }
--- src/scripts/modules/MarketStatus.js
+++ src/scripts/modules/MarketStatus.js
@@ -16,6 +16,8 @@ class MarketStatus{
         this.marketcap = document.querySelector('.market-status__marketcap')
         this.volume = document.querySelector('.market-status__volume')
         this.dominanceElement = document.querySelector('.market-status__dominance')
+        this.marketCapChangeEl = document.querySelector('.marketcap-change__ptc')
+        this.volumeChangeEl = document.querySelector('.volume-change__ptc')
 
         // Sets the total cryptocurrency market cap
         this.callApi(this.marketCapUrl, this.setMarketCap)
@@ -23,7 +25,7 @@ class MarketStatus{
         this.callApi(this.volumeUrl, this.setVolume)
         // Sets the top three dominant coins
         this.callApi(this.coinsURL, this.setDominance)
-
+        
     }
 
     yesterdaysDate(){
@@ -53,15 +55,38 @@ class MarketStatus{
             })
     }
 
-
+    // Adds commas to large numbers
     formatNumber(num){
         return num.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,')
     }
 
+    // Calculate 24 hour change
+    calculateChange(currentNumber, originalNumber){
+        let change = ((currentNumber - originalNumber) / originalNumber * 100).toFixed(2) + '%'
+        return change
+    }
+
+    setChangeColor(change, element){
+        let changeInt = parseFloat(change)
+        if (Math.sign(changeInt) === 1){
+            element.style.color = 'green'
+        }else if (Math.sign(changeInt) === -1){
+            element.style.color = 'red'
+        }else{
+            element.style.color = '#757575'
+        }
+    }
+
     setMarketCap(data){
-        let totalMarketcap = data[22].market_cap
+        let lastItemInArray = data.length - 1
+        let totalMarketcap = data[lastItemInArray].market_cap
         this.marketcap.innerHTML = `$ ${this.formatNumber(totalMarketcap)}`
         this.totalMarketCap = totalMarketcap
+
+        // Set 24 hour market cap change in percentage
+        let change = this.calculateChange(totalMarketcap, data[0].market_cap)
+        this.marketCapChangeEl.innerHTML = change
+        this.setChangeColor(change, this.marketCapChangeEl)
         return totalMarketcap
     }
 
@@ -72,11 +97,20 @@ class MarketStatus{
             totalVolume = totalVolume + parseInt(data[i].volume)
         }
         this.volume.innerHTML = `$ ${this.formatNumber(totalVolume)}`
+    
+        // Set 24 hour volume change in percentage
+        let lastItemInArray = data.length - 1
+        let change = this.calculateChange(data[lastItemInArray].volume, data[0].volume)
+        this.volumeChangeEl.innerHTML = change
+        this.setChangeColor(change, this.volumeChangeEl)
+        console.log('Data length is ' + lastItemInArray)
     }
 
     setDominance(data){
         let topThree = []
+        let ol = this.dominanceElement
 
+        ol.innerHTML = ''
         for(let i = 0; i < 3; i++){
             let coinMarketCap = parseInt(data[i].market_cap)
             topThree[i] = {
@@ -84,7 +118,6 @@ class MarketStatus{
                 dominance: ((coinMarketCap / this.totalMarketCap) * 100).toFixed(2) + '%'
             }
 
-            let ol = this.dominanceElement
             let li = document.createElement('li')
             let textNode = document.createTextNode(topThree[i].coinSymbol + '\xa0\xa0\xa0\xa0' + topThree[i].dominance)
             li.appendChild(textNode)
