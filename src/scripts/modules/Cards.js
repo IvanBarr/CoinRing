3f69f0f7ec48ff071d562542dd5896340d1422bc
IvanBarr <ivanbar.webdev@gmail.com>
2020-02-13 00:32:39 -0800

    worked on responsive design and refactored javascript code for cards

--- src/scripts/modules/Cards.js
+++ src/scripts/modules/Cards.js
@@ -4,68 +4,86 @@ class Cards{
     constructor(){
         this.apiUrl = `https://api.nomics.com/v1/currencies/ticker?key=${process.env.API_KEY}&1d`
         this.cards = document.querySelector('.cards')
-        this.cards_wrapper = document.querySelector('.cards__wrapper')
+        this.cards_container = document.querySelector('.cards__container')
         this.makeCard()
     }
+
     formatNumber(num){
         return num.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,')
+    }
+
+    abbreviateNumber(value){
+        let newValue = value;
+        const suffixes = ["", "K", "M", "B","T"];
+        let suffixNum = 0;
+        while (newValue >= 1000) {
+            newValue /= 1000;
+            suffixNum++;
+        }
+
+        newValue = newValue.toPrecision(3);
+
+        newValue += suffixes[suffixNum];
+        return newValue;
     }
 
     makeCard(){
         fetch(this.apiUrl)
             .then(res => res.json())
             .then(data => {
-                this.cards_wrapper.innerHTML = ''
+                this.cards_container.innerHTML = ''
                 for(let i = 0; i < 100; i++){
                     let markup = `
                         <div class="cards__item">
-                            <div class="item__symbol-price">
-                                <b class="symbol-price__symbol">${data[i].symbol}</b>
-                                <b class="symbol-price__price">${this.formatNumber(parseFloat(data[i].price).toFixed(3))}</b>
-                            </div>
-                            <div class="price-change">
-                                <p>
-                                    ${
-                                        // If data for last 24hrs available then show pct change, if not, show "N/A"
-                                        data[i]['1d'] ? (data[i]['1d']['price_change_pct'] * 100).toFixed(2) + '%' : 'N/A'
-                                    }
-                                </p>
-                            </div>
-                            <div class="price-change">
-                                <p>
-                                    ${
-                                        // If data for last 7 days available then show pct change, if not, show "N/A"
-                                        data[i]['1d'] ? (data[i]['7d']['price_change_pct'] * 100).toFixed(2) + '%' : 'N/A'
-                                    }
-                                </p>
-                            </div>
-                            <div class="price-change">
-                                <p>
-                                    ${
-                                        // If data for Year To Date available then show pct change, if not, show "N/A"
-                                        data[i]['ytd'] ? (data[i]['ytd']['price_change_pct'] * 100).toFixed(2) + '%' : 'N/A'
-                                    }
-                                </p>
-                            </div>
-                            <div class="market-cap">
-                                <p>
-                                    ${
-                                        // If data for last 7 days available then show pct change, if not, show "N/A"
-                                        data[i].market_cap ? this.formatNumber(data[i].market_cap) : 'N/A'
-                                    }
-                                </p>
-                            </div>
-                            <div class="volume">
-                                <p>
-                                    ${
-                                        // If data for last 7 days available then show pct change, if not, show "N/A"
-                                        data[i]['1d'] ? this.formatNumber(data[i]['1d']['volume']) : 'N/A'
-                                    }
-                                </p>
+                            <div class="wrapper">
+                                <div class="item__symbol-price">
+                                    <b class="symbol-price__symbol">${data[i].symbol}</b>
+                                    <b class="symbol-price__price">${this.formatNumber(parseFloat(data[i].price).toFixed(3))}</b>
+                                </div>
+                                <div class="price-change">
+                                    <p>
+                                        ${
+                                            // If data for last 24hrs available then show pct change, if not, show "N/A"
+                                            data[i]['1d'] ? (data[i]['1d']['price_change_pct'] * 100).toFixed(2) + '%' : 'N/A'
+                                        }
+                                    </p>
+                                </div>
+                                <div class="price-change">
+                                    <p>
+                                        ${
+                                            // If data for last 7 days available then show pct change, if not, show "N/A"
+                                            data[i]['1d'] ? (data[i]['7d']['price_change_pct'] * 100).toFixed(2) + '%' : 'N/A'
+                                        }
+                                    </p>
+                                </div>
+                                <div class="price-change">
+                                    <p>
+                                        ${
+                                            // If data for Year To Date available then show pct change, if not, show "N/A"
+                                            data[i]['ytd'] ? (data[i]['ytd']['price_change_pct'] * 100).toFixed(2) + '%' : 'N/A'
+                                        }
+                                    </p>
+                                </div>
+                                <div class="market-cap">
+                                    <p>
+                                        ${
+                                            // If data for last 7 days available then show pct change, if not, show "N/A"
+                                            data[i].market_cap ? this.abbreviateNumber(data[i].market_cap) : 'N/A'
+                                        }
+                                    </p>
+                                </div>
+                                <div class="volume">
+                                    <p>
+                                        ${
+                                            // If data for last 7 days available then show pct change, if not, show "N/A"
+                                            data[i]['1d'] ? this.abbreviateNumber(data[i]['1d']['volume']) : 'N/A'
+                                        }
+                                    </p>
+                                </div>
                             </div>
                         </div>
                     `
-                    this.cards_wrapper.insertAdjacentHTML("beforeend", markup)
+                    this.cards_container.insertAdjacentHTML("beforeend", markup)
                 }
                 console.log(data[0])
             })
