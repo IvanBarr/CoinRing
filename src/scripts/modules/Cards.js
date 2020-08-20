1645ca11f4a1ab426ca32d68116b690b3caf7d43
ivan <ivanbar.webdev@gmail.com>
2020-08-19 21:56:34 -0700

    finished working on add to favorites functionality and added a new _favorites.scss file

--- src/scripts/modules/Cards.js
+++ src/scripts/modules/Cards.js
@@ -8,12 +8,6 @@ class Cards{
         this.cards_container = document.querySelector('.cards__container')
         this.favorites = JSON.parse(localStorage.getItem("coin"))
         this.makeCard()
-        this.events()
-    }
-
-    events(){
-        // this.cards_container.addEventListener('click', this.addToFavorites.bind(this))
-       
     }
 
     formatNumber(num){
@@ -51,14 +45,27 @@ class Cards{
                 priceChangePct[i].style.color = ""
             }
         }
-        // console.log(priceChangeContainer[1].innerText)
+    }
+
+    setFavoritesBtnStyle(data, p){
+        let addToFavBtn = document.getElementsByClassName("add-favorite_btn")[p]
+        if(this.favorites){
+            this.favorites.map(item => {
+                if(item.symbol === JSON.stringify(data[p].symbol)){
+                    addToFavBtn.className += " fas fa-star"
+                }else{
+                    addToFavBtn.className += " far fa-star"
+                }
+            })
+        }else{
+            addToFavBtn.className += " far fa-star"
+        }
     }
 
     makeCard(){
         fetch(this.apiUrl)
             .then(res => res.json())
             .then(data => {
-                // console.log(this.favorites[0] === data[0].symbol)
                 let markup
                 this.cards_container.innerHTML = ''
                 if(!location.href.includes("favorites.html")){
@@ -115,126 +122,89 @@ class Cards{
                                             }
                                         </p>
                                     </div>
-                                    <i class="add-favorite_btn far fa-star"></i>
+                                    <i class="add-favorite_btn"></i>
                                 </div>
                             </div>
                         `
                         this.cards_container.insertAdjacentHTML("beforeend", markup)
+                        this.setFavoritesBtnStyle(data, i)
                     }
                 }else{
                     if(this.favorites){
-                        for(let i = 0; i < 100; i++){
+                        for(let z = 0; z < 100; z++){
                            for(let x = 0; x < 100; x++){
-                               if(this.favorites[i] === data[x].symbol){
+                               if(JSON.stringify(data[x].symbol) === this.favorites[z].symbol){
                                     markup = `
-                                    <div class="cards__item">
-                                        <div class="wrapper">
-                                            <div class="item__symbol-price">
-                                                <b class="symbol-price__symbol">${x+1 + '. ' + data[x].symbol}</b>
-                                                <b class="symbol-price__price">$${this.formatNumber(parseFloat(data[x].price).toFixed(3))}</b>
+                                        <div class="cards__item">
+                                            <div class="wrapper">
+                                                <div class="item__symbol-price">
+                                                    <b class="symbol-price__symbol">${x+1 + '. ' + data[x].symbol}</b>
+                                                    <b class="symbol-price__price">$${this.formatNumber(parseFloat(data[x].price).toFixed(3))}</b>
+                                                </div>
+                                                <div class="price-change">
+                                                    <b class="price-change-timeframe">1hr</b>
+                                                    <p class="price-change-pct">
+                                                        ${
+                                                            // If data for last 24hrs available then show pct change, if not, show "N/A"
+                                                            data[x]['1d'] ? (data[x]['1d']['price_change_pct'] * 100).toFixed(2) + '%' : 'N/A'
+                                                        }
+                                                    </p>
+                                                </div>
+                                                <div class="price-change">
+                                                    <b class="price-change-timeframe">24hr</b>
+                                                    <p class="price-change-pct">
+                                                        ${
+                                                            // If data for last 7 days available then show pct change, if not, show "N/A"
+                                                            data[x]['1d'] ? (data[x]['7d']['price_change_pct'] * 100).toFixed(2) + '%' : 'N/A'
+                                                        }
+                                                    </p>
+                                                </div>
+                                                <div class="price-change">
+                                                    <b class="price-change-timeframe">Year To Date</b>
+                                                    <p class="price-change-pct">
+                                                        ${
+                                                            // If data for Year To Date available then show pct change, if not, show "N/A"
+                                                            data[x]['ytd'] ? (data[x]['ytd']['price_change_pct'] * 100).toFixed(2) + '%' : 'N/A'
+                                                        }
+                                                    </p>
+                                                </div>
+                                                <div class="market-cap">
+                                                    <b class="price-change-timeframe">Marketcap</b>
+                                                    <p>
+                                                        ${
+                                                            // If data for last 7 days available then show pct change, if not, show "N/A"
+                                                            data[x].market_cap ? this.abbreviateNumber(data[x].market_cap) : 'N/A'
+                                                        }
+                                                    </p>
+                                                </div>
+                                                <div class="volume">
+                                                    <b class="price-change-timeframe">Volume</b>
+                                                    <p>
+                                                        ${
+                                                            // If data for last 7 days available then show pct change, if not, show "N/A"
+                                                            data[x]['1d'] ? this.abbreviateNumber(data[x]['1d']['volume']) : 'N/A'
+                                                        }
+                                                    </p>
+                                                </div>
+                                                <i class="add-favorite_btn fas fa-star"></i>
                                             </div>
-                                            <div class="price-change">
-                                                <b class="price-change-timeframe">1hr</b>
-                                                <p class="price-change-pct">
-                                                    ${
-                                                        // If data for last 24hrs available then show pct change, if not, show "N/A"
-                                                        data[x]['1d'] ? (data[x]['1d']['price_change_pct'] * 100).toFixed(2) + '%' : 'N/A'
-                                                    }
-                                                </p>
-                                            </div>
-                                            <div class="price-change">
-                                                <b class="price-change-timeframe">24hr</b>
-                                                <p class="price-change-pct">
-                                                    ${
-                                                        // If data for last 7 days available then show pct change, if not, show "N/A"
-                                                        data[x]['1d'] ? (data[x]['7d']['price_change_pct'] * 100).toFixed(2) + '%' : 'N/A'
-                                                    }
-                                                </p>
-                                            </div>
-                                            <div class="price-change">
-                                                <b class="price-change-timeframe">Year To Date</b>
-                                                <p class="price-change-pct">
-                                                    ${
-                                                        // If data for Year To Date available then show pct change, if not, show "N/A"
-                                                        data[x]['ytd'] ? (data[x]['ytd']['price_change_pct'] * 100).toFixed(2) + '%' : 'N/A'
-                                                    }
-                                                </p>
-                                            </div>
-                                            <div class="market-cap">
-                                                <b class="price-change-timeframe">Marketcap</b>
-                                                <p>
-                                                    ${
-                                                        // If data for last 7 days available then show pct change, if not, show "N/A"
-                                                        data[x].market_cap ? this.abbreviateNumber(data[x].market_cap) : 'N/A'
-                                                    }
-                                                </p>
-                                            </div>
-                                            <div class="volume">
-                                                <b class="price-change-timeframe">Volume</b>
-                                                <p>
-                                                    ${
-                                                        // If data for last 7 days available then show pct change, if not, show "N/A"
-                                                        data[x]['1d'] ? this.abbreviateNumber(data[x]['1d']['volume']) : 'N/A'
-                                                    }
-                                                </p>
-                                            </div>
-                                            <i class="add-favorite_btn far fa-star"></i>
                                         </div>
-                                    </div>
-                                `
-                                this.cards_container.insertAdjacentHTML("beforeend", markup)
+                                    `
+                                    this.cards_container.insertAdjacentHTML("beforeend", markup)
+                                    this.setPriceChangeBgColor()
                                }
                            }
                         }
                         
                     }else{
-                        alert('nothing here!')
+                        this.cards_container.insertAdjacentHTML("beforeend", "<h1 class='favorites_msg'>You haven't added anything yet!</h1>")
                     }
                 }
                 this.setPriceChangeBgColor()
-                // let cardItem = document.querySelector('.add-favorite_btn')
-                // addToFavoriteBtn.addEventListener('click', () => {console.log('hi')})
             })
             .catch(err => {
                 console.error(err)
             })
-    }
-    
-    addToFavorites(e){
-        let target = e.target
-        if (target.className != "add-favorite_btn far fa-star") return
-
-        // this.favorites.push(target.parentNode.childNodes[1].childNodes[1].innerText.replace(/[\d.' ']/g, ''))
-         
-        // if(localStorage.getItem("coin") != null && this.favorites.length === 0){
-        //     this.favorites = JSON.parse(localStorage.getItem("coin"))
-        //     this.favorites.push(target.parentNode.childNodes[1].childNodes[1].innerText.replace(/[\d.' ']/g, ''))
-        //     localStorage.setItem("coin", JSON.stringify(this.favorites))
-        //     console.log(this.favorites + '' + '1st if')
-            
-        // }else if(localStorage.getItem("coin") === null && this.favorites.length === 0){
-        //     this.favorites.push(target.parentNode.childNodes[1].childNodes[1].innerText.replace(/[\d.' ']/g, ''))
-        //     localStorage.setItem("coin", JSON.stringify(this.favorites))
-        //     console.log(this.favorites + '' + '2nd if')
-        // }else if(this.favorites.length){
-        //     localStorage.clear()
-        //     this.favorites.push(target.parentNode.childNodes[1].childNodes[1].innerText.replace(/[\d.' ']/g, ''))
-        //     localStorage.setItem("coin", JSON.stringify(this.favorites))
-        //     console.log(this.favorites + '' + '3rd if')
-        // }
-
-
-        // console.log(localStorage.getItem("coin"))
-
-        // if(localStorage.getItem("coin") === null){
-        //     this.favorites.push(target.parentNode.childNodes[1].childNodes[1].innerText.replace(/[\d.' ']/g, ''))
-        //     localStorage.setItem("coin", JSON.stringify(this.favorites))
-        // }else{
-        //     this.favorites = JSON.parse(localStorage.getItem("coin"))
-        //     this.favorites.push(target.parentNode.childNodes[1].childNodes[1].innerText.replace(/[\d.' ']/g, ''))
-        //     localStorage.setItem("coin", JSON.stringify(this.favorites))
-        // }
-        // console.log('hi')
     }
 }
 
--- src/scripts/modules/Favorites.js
+++ src/scripts/modules/Favorites.js
@@ -15,38 +15,51 @@ class Favorites{
 
     addToFavorites(e){
         let target = e.target
-        if (target.className != "add-favorite_btn far fa-star") return
+        if (target.classList.contains("add-favorite_btn")){
 
-        let itemClicked = target.parentNode.childNodes[1].childNodes[1].innerText.replace(/[\d.' ']/g, '')
 
-        if(localStorage.getItem("coin") != null){
-            this.favorites = JSON.parse(localStorage.getItem("coin"))
-                if(!this.favorites.includes(itemClicked)){
-                    // target.parentNode.childNodes[13].classList.remove("far")
-                    // target.parentNode.childNodes[13].classList.add("fas")
-                    this.favorites.push(itemClicked)
-                    localStorage.setItem("coin", JSON.stringify(this.favorites))
-                }else{
-                    // target.parentNode.childNodes[13].classList.remove("fas")
-                    // target.parentNode.childNodes[13].classList.add("far")
-                    let itemIndex = this.favorites.indexOf(itemClicked)
-                    let filteredFavorites = this.favorites.filter(item => item !== itemClicked)
-                    this.favorites = filteredFavorites
-                    localStorage.setItem("coin", JSON.stringify(this.favorites))
+            let itemClicked = JSON.stringify(target.parentNode.childNodes[1].childNodes[1].innerText.replace(/[\d.' ']/g, ''))
+
+            // If localStorage HAS items
+            if(localStorage.getItem("coin") != null){
+                this.favorites = JSON.parse(localStorage.getItem("coin"))
+                // If localStorage Doesn't have the item clicked on
+                    if(this.favorites.some(el => el.symbol === itemClicked) === false){
+                        let newObj = {
+                            symbol: itemClicked,
+                            favorited: true
+                        }
+                        this.favorites.push(newObj)
+                        localStorage.setItem("coin", JSON.stringify(this.favorites))
+                        console.log(this.favorites)
+                    }else{
+                        if(this.favorites.length === 1){
+                            localStorage.clear()
+                            this.favorites = []
+     
+                        }else{
+                             // If localStorage HAS the item clicked on...REMOVE IT
+                            let filteredFavorites = this.favorites.filter(item => item.symbol !== itemClicked)
+                            this.favorites = filteredFavorites
+                            localStorage.setItem("coin", JSON.stringify(this.favorites))
+                            console.log(this.favorites)
+                        }
+                    }
+            }else{ // If localStorage is EMPTY
+                let newObj = {
+                    symbol: itemClicked,
+                    favorited: true
                 }
-        }else{
-            // target.parentNode.childNodes[13].classList.remove("far")
-            // target.parentNode.childNodes[13].classList.add("fas")
-            this.favorites.push(itemClicked)
-            localStorage.setItem("coin", JSON.stringify(this.favorites))
-            // console.log(target.parentNode.childNodes[13].classList)
+
+                this.favorites.push(newObj)
+                localStorage.setItem("coin", JSON.stringify(this.favorites))
+                console.log(this.favorites)
+            }
+
+
+            // console.log(this.favorites)
         }
-
-
-        // console.log(this.favorites)
+        new Cards()
     }
 }
 export default Favorites
-// let itemIndex = this.favorites.indexOf(itemClicked)
-// let removedItem = this.favorites.splice(itemIndex, 1)
-// console.log(this.favorites)
